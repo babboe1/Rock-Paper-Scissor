1c6893bc61d23daa77f5d07b35888e67c7bb1602
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-09-29 23:11:54 +0100

    feat: add form to get player choice

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = 'ROCK';
 
 const getPlayerChoice = function () {
-   const selection = prompt('Pock, Paper or scissors?');
-   if (selection ) {
-      
+   const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase;
+   if (
+      selection !== ROCK &&
+      selection !== PAPER &&
+      selection !== SCISSORS
+      ) {
+      alert( `invalid choice !!, we chose ${DEFAULT_USER_CHOICE} for you)`);
+      return ROCK;
+   }
+   {
+      return selection;
    }
 };
 function startGame() {
