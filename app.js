ed811ec02560e55845b6dd8551e8c656ff9b5797
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-09-29 20:35:23 +0100

    feat: create player choice

---
+++ app.js
@@ -0,0 +1,13 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const getPlayerChoice = function () {
+   const selection = prompt('Pock, Paper or scissors?');
+   if (selection ) {
+      
+   }
+};
+function startGame() {
+   console.log('Game is starting....');
+}
+
+startGameBtn.addEventListener('click', startGame);
