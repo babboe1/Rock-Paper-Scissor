4d4a0c2b937cd8eaa1e1e29fcc27c91d3cd3675f
Ayodele Olalekan <longyarnz@gmail.com>
2021-10-27 12:19:07 +0100

    chore: comments

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ const DEFAULT_USER_CHOICE = 'ROCK';
 const RESULT_DRAW = 'DRAW';
 const RESULT_PLAYER_WIN = 'PLAYER_WINS';
 const RESULT_PLAYER_LOSE = 'PLAYER_LOST';
-let gameIsRunning = false;
+// let gameIsRunning = false;
+
+
 
 const getPlayerChoice = function () {
-   const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
-   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-      alert(`invalid choice !!, we chose ${DEFAULT_USER_CHOICE} for you)`);
+   // const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
+   // if ((selection !== ROCK && selection !== PAPER && selection !== SCISSORS)) {
+   //    alert(`invalid choice !!, we chose ${DEFAULT_USER_CHOICE} for you)`);
+   //    return ROCK;
+   // } \
+   if (rockBtn.click) {
       return ROCK;
    }
    {
